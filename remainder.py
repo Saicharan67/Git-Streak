f91ca7d5983285a8dbac2a19a214e0cd3c12505b
Saicharan67 <mahankalisaicharan@gmail.com>
2020-12-13 19:25:21 +0530

    success using smtp

--- remainder.py
+++ remainder.py
@@ -3,29 +3,34 @@ from datetime import datetime as date
 import schedule
 import requests
 import smtplib
+
 page = requests.get("https://github.com/Saicharan67")
-soup = BeautifulSoup(page.content, 'html.parser')
+soup = BeautifulSoup(page.content, "html.parser")
+
+
 def SendMail():
-    
 
-    host = "server.smtp.com"
-    server = smtplib.SMTP(host)
-    FROM = "611848@student.nitandhra.ac.in"
-    TO = "mahankalisaicharan@gmail.com"
-    MSG = "Subject: Test email python\n\nBody of your message!"
-    server.sendmail(FROM, TO, MSG)
+    s = smtplib.SMTP("smtp.gmail.com", 587)
+    s.starttls()
+    s.login("gachibowlydiwalkar@gmail.com", "gachibowly@02")
+    message = "Your GitHub streak is about to break. Go and make a commit quick!"
+    s.sendmail("gachibowlydiwalkar@gmail.com", "mahankalisaicharan@gmail.com", message)
 
-    server.quit()
-    print ("Email Send")
+    print("sent")
+    s.quit()
+
+
 def EmailStreak():
     Date = date.today()
-    Todays_Date= str(Date.year)+'-'+str(Date.month)+'-'+str(Date.day)
-    Todays_Streak = soup.find_all('rect',attrs={'data-date':Todays_Date})
+    Todays_Date = str(Date.year) + "-" + str(Date.month) + "-" + str(Date.day)
+    Todays_Streak = soup.find_all("rect", attrs={"data-date": Todays_Date})
+    if Todays_Streak != 0:
+        SendMail()
+    # print(Todays_Streak[0]["data-count"])
 
-    SendMail()
-    print(Todays_Streak[0]['data-count'])
 
-schedule.every().day.at("19:00").do(EmailStreak)
+schedule.every().day.at("19:22").do(EmailStreak)
 while True:
     schedule.run_pending()
-
+# EmailStreak()
+#!/usr/bin/python
