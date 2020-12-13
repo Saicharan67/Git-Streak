1ac7787675defff6b9c31ada61bc482c3a386d91
Saicharan67 <mahankalisaicharan@gmail.com>
2020-12-13 15:58:08 +0530

    success using bs4

--- remainder.py
+++ remainder.py
@@ -1,22 +1,14 @@
-# from bs4 import BeautifulSoup
-# import requests
-# page = requests.get("https://github.com/Saicharan67")
-# soup = BeautifulSoup(page.content, 'html.parser')
-# print(soup.find_all('svg'))
-
-
+from bs4 import BeautifulSoup
 from datetime import datetime as date
-from selenium import webdriver
-import time
-from selenium.webdriver.common.keys import Keys
 
-# path to your chrome driver
-cd = 'C:\\Users\\hp\\chromedriver.exe'
+import requests
+page = requests.get("https://github.com/Saicharan67")
+soup = BeautifulSoup(page.content, 'html.parser')
+
 
 Date = date.today()
 Todays_Date= str(Date.year)+'-'+str(Date.month)+'-'+str(Date.day)
-browser = webdriver.Chrome(cd)
-browser.get("https://github.com/Saicharan67")
+Todays_Streak = soup.find_all('rect',attrs={'data-date':Todays_Date})
 
-Todays_Streak = browser.find_element_by_xpath("//*[@data-date='{0}']".format(Todays_Date))
-print(Todays_Streak.get_attribute('data-count'))
+
+print(Todays_Streak[0]['data-count'])
