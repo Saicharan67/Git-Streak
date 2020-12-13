98cc7179b57b0ad67fe2db6ae7e9d63c1aa47139
Saicharan67 <mahankalisaicharan@gmail.com>
2020-12-13 16:24:18 +0530

    success using selenium

--- remainder.py
+++ remainder.py
@@ -5,10 +5,14 @@ import requests
 page = requests.get("https://github.com/Saicharan67")
 soup = BeautifulSoup(page.content, 'html.parser')
 
+def EmailStreak():
+    Date = date.today()
+    Todays_Date= str(Date.year)+'-'+str(Date.month)+'-'+str(Date.day)
+    Todays_Streak = soup.find_all('rect',attrs={'data-date':Todays_Date})
 
-Date = date.today()
-Todays_Date= str(Date.year)+'-'+str(Date.month)+'-'+str(Date.day)
-Todays_Streak = soup.find_all('rect',attrs={'data-date':Todays_Date})
 
+    return Todays_Streak[0]['data-count']
 
-print(Todays_Streak[0]['data-count'])
+schedule.every().day.at("19:00").do(EmailStreak)
+while True:
+    schedule.run_pending()
